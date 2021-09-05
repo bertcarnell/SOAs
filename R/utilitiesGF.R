c4a63a7f20190556b72721938f3fc50400e7cf2d
Rob Carnell <bertcarnell@gmail.com>
2021-09-05 13:59:10 -0400

    Updated documentation.  Added GF functions documenation and tests

--- R/utilitiesGF.R
+++ R/utilitiesGF.R
@@ -1,21 +1,43 @@
+#' Integer to Galois field polynomial
+#'
+#' @param x integer vector with e elements
+#' @param gf Galois field object
+#'
+#' @return e x n matrix or length n vector (if x is scalar)
+#'
+#' @keywords internal
 int2poly <- function(x, gf){
-  ## takes integer vector x with e elements
-  ## returns e x n matrix or length n vector (if x is scalar)
   gf$poly[x+1, , drop=FALSE]
 }
 
+
+#' Galois field polynomial to an integer
+#'
+#' @param poly takes e x n matrix of polygon rows or a
+#' length n vector for a single polygon (e=1)
+#' @param gf Galois field object
+#'
+#' @return e element vector of integers
+#'
+#' @keywords internal
 poly2int <- function(poly, gf){
-  ## takes e x n matrix of polygon rows
-  ## or a length n vector for a single polygon (e=1)
-  ## returns e element vector of integers
   #stopifnot("GaloisField" %in% class(gf))
   p <- gf$p; n <- gf$n
   stopifnot(all(poly %in% 0:(p-1)))
-  if (!is.null(dim(poly))) stopifnot(ncol(poly)==n) else 
+  if (!is.null(dim(poly))) stopifnot(ncol(poly)==n) else
     stopifnot(length(poly)==n)
   poly%*%c(p^(0:(n-1)))
 }
 
+#' Sum of a two elements of a Galois field
+#'
+#' @param x first element in \code{0:(q-1)}
+#' @param y second element in \code{0:(q-1)}
+#' @param gf Galois field of characteristic \code{q}
+#'
+#' @return the sum
+#'
+#' @keywords internal
 gf_sum <- function(x, y, gf){
   #stopifnot("GaloisField" %in% class(gf))
   #q <- gf$q
@@ -25,6 +47,15 @@ gf_sum <- function(x, y, gf){
     gf$plus[x[obj]+1, y[obj]+1])
 }
 
+#' Product of two elements of a Galois field
+#'
+#' @param x the first element in \code{0:(q-1)}
+#' @param y the second element in \code{0:(q-1)}
+#' @param gf the Galois field of characteristic \code{q}
+#'
+#' @return the product
+#'
+#' @keywords internal
 gf_prod <- function(x, y, gf){
   #stopifnot("GaloisField" %in% class(gf))
   #q <- gf$q
@@ -34,23 +65,42 @@ gf_prod <- function(x, y, gf){
     gf$times[x[obj]+1, y[obj]+1])
 }
 
-gf_sum_list <- function (ll, gf, checks = TRUE) 
+#' Sum over a list of integer vectors in a Galois field
+#'
+#' @param ll a list of integer vectors in \code{0:(q-1)}
+#' @param gf a Galois field of characteristic \code{q}
+#' @param checks should input checks be performed
+#'
+#' @return a integer vector
+#'
+#' @keywords internal
+gf_sum_list <- function (ll, gf, checks = TRUE)
 {
   ## ll is a list of integer vectors to be summed over gf
   if (checks) {
     stopifnot("GaloisField" %in% class(gf))
-    if (!all(c(unlist(ll)) %in% 0:(gf$q - 1))) 
+    if (!all(c(unlist(ll)) %in% 0:(gf$q - 1)))
       stop("invalid numbers occur in ll")
-    if (!length(unique(lengths(ll))) == 1) 
+    if (!length(unique(lengths(ll))) == 1)
       stop("all elements of ll must have the same length")
-    
+
   }
   hilf <- lapply(ll, int2poly, gf=gf)
   hilf <- base::Reduce("+", hilf)%%gf$p
   apply(hilf, 1, function(obj) lhs::poly2int(gf$p, gf$n, obj))
 }
 
-gf_matmult <- function (M1, M2, gf, checks = TRUE) 
+#' Multiple matrices over Galois fields
+#'
+#' @param M1 matrix 1 with elements in \code{0:(q-1)}
+#' @param M2 matrix 2 with elements in \code{0:(q-1)}
+#' @param gf Galois field object with characteristic \code{q}
+#' @param checks should checks be performed
+#'
+#' @return an integer matrix
+#'
+#' @keyword internal
+gf_matmult <- function (M1, M2, gf, checks = TRUE)
 {
   if (checks) {
     stopifnot("GaloisField" %in% class(gf))
---
+++ tests/testthat/test-utilitiesGF.R
@@ -0,0 +1,79 @@
+gf <- lhs::create_galois_field(9)
+
+expect_all_equal <- function(x, y, ...)
+{
+  temp <- mapply(function(x, y) {expect_equal(x, y, ...)}, x, y)
+  invisible(temp)
+}
+
+test_that("int2poly", {
+  expect_all_equal(int2poly(0, gf), c(0,0))
+  expect_all_equal(int2poly(0, gf), c(0,0))
+  expect_all_equal(int2poly(1, gf), c(1,0))
+  expect_all_equal(int2poly(2, gf), c(2,0))
+  expect_all_equal(int2poly(3, gf), c(0,1))
+  expect_all_equal(int2poly(4, gf), c(1,1))
+  expect_all_equal(int2poly(5, gf), c(2,1))
+  expect_all_equal(int2poly(6, gf), c(0,2))
+  expect_all_equal(int2poly(7, gf), c(1,2))
+  expect_all_equal(int2poly(8, gf), c(2,2))
+
+  expect_equal(int2poly(c(1,4,7), gf), matrix(c(1,0,1,1,1,2), nrow = 3, byrow=TRUE))
+
+  expect_error(int2poly(9, gf))
+})
+
+test_that("poly2int", {
+  expect_equal(c(poly2int(c(0,0), gf)), 0)
+  expect_equal(c(poly2int(c(1,0), gf)), 1)
+  expect_equal(c(poly2int(c(2,0), gf)), 2)
+  expect_equal(c(poly2int(c(0,1), gf)), 3)
+  expect_equal(c(poly2int(c(0,2), gf)), 6)
+
+  expect_all_equal(c(poly2int(as.matrix(expand.grid(0:2, 0:2)), gf)), 0:8)
+})
+
+test_that("gf_sum", {
+  expect_equal(gf_sum(1, 2, gf), 0)
+  expect_equal(gf_sum(6, 8, gf), 5)
+  expect_all_equal(gf_sum(c(3,5), c(0,1), gf), c(3, 3))
+})
+
+test_that("gf_prod", {
+  expect_equal(gf_prod(0, 2, gf), 0)
+  expect_equal(gf_prod(1, 2, gf), 2)
+  expect_equal(gf_prod(3, 2, gf), 6)
+  expect_equal(gf_prod(1, 3, gf), 3)
+  expect_equal(gf_prod(8, 8, gf), 5)
+  expect_all_equal(gf_prod(c(0,1,3), c(2,2,2), gf), c(0, 2, 6))
+})
+
+test_that("gf_sum_list", {
+  expect_all_equal(gf_sum_list(list(a = c(1,0), b=c(2,5), d=c(0,8)), gf, checks = TRUE), c(0, 1))
+  temp <- gf_sum_list(list(a = rep(0:8, each = 81),
+                           b=rep(0:8, each = 9, times = 9),
+                           d=rep(0:8, times = 81)), gf, checks = TRUE)
+  counter <- 1
+  for (i in 0:8)
+  {
+    for (j in 0:8)
+    {
+      for (k in 0:8)
+      {
+        expect_true(gf_sum(gf_sum(i, j, gf), k, gf) == temp[counter])
+        counter <- counter + 1
+      }
+    }
+  }
+})
+
+test_that("gf_matmult", {
+  M1 <- matrix(0:8, ncol = 1)
+  M2 <- matrix(0:8, nrow = 1)
+  expect_all_equal(c(gf_matmult(M1, M2, gf, checks = TRUE)), c(gf$times))
+
+  M1 <- matrix(0:3, ncol = 2, nrow = 2)
+  M2 <- matrix(0:3, ncol = 2, nrow = 2)
+  expect_all_equal(c(gf_matmult(M1, M2, gf, checks = TRUE)), c(2, 3, 6, 6))
+})
+
