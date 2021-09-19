8c849cb330f85f2dcb11db0e10fa6f54321e8bc8
Rob Carnell <bertcarnell@gmail.com>
2021-09-19 15:03:20 -0400

    Increase code coverage in OSOA_LiuLiut

--- tests/testthat/test-OSOA_LiuLiut.R
+++ tests/testthat/test-OSOA_LiuLiut.R
@@ -7,6 +7,22 @@ test_that("OSOA_LiuLiut", {
   expect_error(OSOA_LiuLiut(DoE.base::L16.4.5, t = 4, m = 4))
 
   # test the oa as a data.frame
-  temp <- OSOA_LiuLiut(as.data.frame(lhs::createBose(4, 4)))
+  oa <- as.data.frame(lhs::createBose(4, 4))
+  temp <- OSOA_LiuLiut(oa)
   expect_equal(dim(temp), c(16, 4))
+
+  # test a oa data.frame with a factor
+  oa[,2] <- as.factor(oa[,2])
+  temp <- OSOA_LiuLiut(oa)
+  expect_equal(dim(temp), c(16, 4))
+
+  # test a null t gets a t==3
+  oa <- lhs::createBusht(8, 5, 3)
+  temp <- OSOA_LiuLiut(oa)
+  expect_equal(dim(temp), c(512, 2))
+
+  # test a null t with strength 4 oa gets t==4
+  oa <- lhs::createBusht(5, 4, 4)
+  temp <- OSOA_LiuLiut(oa)
+  expect_equal(dim(temp), c(625, 2))
 })
