5d4ba569c939ba5d5c9e62d9f816fe2cdd2c59da
Rob Carnell <bertcarnell@gmail.com>
2021-09-19 15:28:37 -0400

    Increased Code Coverage in OSOAs-regular

--- tests/testthat/test-OSOAs-regular.R
+++ tests/testthat/test-OSOAs-regular.R
@@ -15,4 +15,13 @@ test_that("OSOAs_regular", {
 
   expect_error(OSOAs_regular(s=3, k=3, el=2, m=5),
                regexp="m is too large", fixed=TRUE)
+
+  expect_error(OSOAs_regular(s = 4, k = 3, el = 3, m = 5),
+               regexp="m is too large in combination with el=3")
+
+  temp <- OSOAs_regular(s = 3, k = 3, m = 3, el = 2, optimize = FALSE)
+  expect_equal(dim(temp), c(27, 3))
+
+  temp <- OSOAs_regular(s = 3, k = 3, m = 3, el = 3, optimize = FALSE)
+  expect_equal(dim(temp), c(27, 3))
 })
