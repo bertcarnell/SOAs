2f3ade5b1843185e54108baed8d8bd85500ab2f4
ugroempi <groemping@beuth-hochschule.de>
2021-09-19 23:54:04 +0200

    added a snapshot test for OSOAs_regular

--- tests/testthat/test-OSOAs-regular.R
+++ tests/testthat/test-OSOAs-regular.R
@@ -6,7 +6,7 @@ test_that("OSOAs_regular", {
   expect_equal(dim(temp), c(27, 4))
   expect_equal(length(unique(c(temp))), 27)
 
-  temp <- OSOAs_regular(s=3, k=3, el=2, optimize=FALSE)
+  expect_snapshot_output((temp <- OSOAs_regular(s=3, k=3, el=2, optimize=FALSE)))
   expect_s3_class(temp, "SOA")
   expect_equal(attr(temp, "type"), "OSOA")
   expect_equal(attr(temp, "strength"), "2+")
