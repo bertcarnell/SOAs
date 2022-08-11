9005a34b1053db1464994098e5c7bd066b8b85ed
ugroempi <ugroempi@users.noreply.github.com>
2022-08-11 15:41:38 +0200

    added a print method for class Spattern

--- R/print-methods.R
+++ R/print-methods.R
@@ -4,15 +4,16 @@
 #' @method print SOA
 #' @return no value is returned
 #'
-#' @param x object to be printed (SOA, OSOA, MDLE)
+#' @param x object to be printed (SOA, OSOA, MDLE, Spattern)
 #' @param ... further arguments for function \code{print}
 #'
 #' @export
 #'
 #' @examples
 #' myOSOA <- OSOAs_regular(s=3, k=3, optimize=FALSE)
-#' print(myOSOA)
+#' myOSOA
 #' str(myOSOA)  ## structure for comparison
+#' Spat <- Spattern(myOSOA, s=3, detailed=TRUE)
 print.SOA <- function(x, ...){
   hilf <- x
   dx <- dim(x)
@@ -36,4 +37,21 @@ print.MDLE <- function(x, ...){
   dimnames(hilf) <- dnx
   print(hilf, ...)
   cat(paste0(attr(x, "type"), " array\n"))
+}
+
+#' @rdname printsoa
+#' @method print Spattern
+#' @export
+print.Spattern <- function(x, ...){
+  objnam <- substitute(x)
+  hilf <- x
+  attrs <- names(attributes(hilf))
+  msg <- ""
+  if ("contribs" %in% attrs){
+    attr(hilf, "contribs") <- NULL
+    attr(hilf, "combis") <- NULL
+    msg <- "Attributes contribs and combis can be accessed using function attr."
+  }
+  print(unclass(hilf), ...)
+  if (nchar(msg)>0) message(msg)
 }
